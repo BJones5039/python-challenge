bd80108b451eb1aea5706ca9d3a3ddcb3fba986a
BJones5039 <bjones5039@gmail.com>
2023-11-14 23:56:30 -0600

    add data files

---
+++ PyBank/main.py
@@ -0,0 +1,21 @@
+#Module
+import os
+
+#read csv
+import csv
+
+#set path for file
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+#open the csv
+with open(csvpath) as csvfile:
+
+    # csv reader specifies delimeter and variable 
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    #read the header row
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
